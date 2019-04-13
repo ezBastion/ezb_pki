ee687eccf377cd9464b001a1ac9e70cd42ec8a73
renaud devers <rdevers@chavers.org>
2019-04-13 17:23:41 +0200

    new agpl world

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+
+**ATTN**: This project uses [semantic versioning](http://semver.org/).
+
+
+
+## 0.1.1 - 2018-07-10
--- service.go
+++ service.go
@@ -1,6 +1,17 @@
-// Copyright 2012 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// This file is part of ezBastion.
+
+//     ezBastion is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+
+//     ezBastion is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+
+//     You should have received a copy of the GNU Affero General Public License
+//     along with ezBastion.  If not, see <https://www.gnu.org/licenses/>.
 
 // +build windows
 
